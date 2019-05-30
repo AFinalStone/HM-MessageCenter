cebb6e17ba356c76aeb061f4fd156218a92e8e2a
石磊 <>
2019-05-30 16:13:14 +0800

    解决BUG

--- library/src/main/java/com/hm/iou/msg/dict/ModuleType.java
+++ library/src/main/java/com/hm/iou/msg/dict/ModuleType.java
@@ -9,8 +9,8 @@ public enum ModuleType {
     CONTRACT_MSG("msg_center_msg_list_header_contract_msg", "合同消息", 100),
     REMIND_BACK_MSG("msg_center_msg_list_header_remind_to_back", "待还提醒", 200),
     SIMILARITY_CONTRACT_MSG("msg_center_msg_list_header_similarity_contract", "疑似合同", 300),
-    HM_MSG("msg_center_msg_list_header_hm_msg", "管家消息", 400),
-    ALIPAY_MSG("msg_center_msg_list_header_alipay_msg", "支付宝回单", 500),
+    HM_MSG("msg_center_msg_list_header_hm_msg", "管家消息", 500),
+    ALIPAY_MSG("msg_center_msg_list_header_alipay_msg", "支付宝回单", 400),
     NEW_APPLY_FRIEND("msg_center_msg_list_new_apply_friend", "新的朋友", 0);
 
     private String typeId;
